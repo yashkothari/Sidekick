a3cb94ae662f0f57f0b34e9915bc9e7c8fd9f491
Ernest Wong <ewong2222@hotmail.com>
2015-03-06 22:47:34 -0500

    initial commit with bluetooth

---
+++ app/src/main/java/com/example/ernestwong/sidekick/BluetoothConnectionReceiver.java
@@ -0,0 +1,26 @@
+package com.example.ernestwong.sidekick;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class BluetoothConnectionReceiver extends BroadcastReceiver {
+    public BluetoothConnectionReceiver(){
+        //No initialisation code needed
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        /*if(BluetoothDevice.ACTION_ACL_CONNECTED.equals(action.getAction())){
+            //Do something with bluetooth device connection
+            log.d("ACTION_ACL_CONNECTED");
+        } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action.getAction())){
+            //Do something with bluetooth device disconnection
+            log.d("ACTION_ACL_DISCONNECTED");
+        } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action.getAction())){
+        } else {
+            log.d("else");
+        }*/
+    }
+}
