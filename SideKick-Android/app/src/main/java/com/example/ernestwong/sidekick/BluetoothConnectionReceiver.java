2a4b3efc40bc449344e13e9fc0771163bdca2f30
Ernest Wong <ewong2222@hotmail.com>
2015-03-07 16:58:27 -0500

    can pair bluetooth

--- SideKick-Android/app/src/main/java/com/example/ernestwong/sidekick/BluetoothConnectionReceiver.java
+++ SideKick-Android/app/src/main/java/com/example/ernestwong/sidekick/BluetoothConnectionReceiver.java
@@ -1,18 +1,27 @@
 package com.example.ernestwong.sidekick;
+
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.bluetooth.*;
-
+import android.util.Log;
 
 public class BluetoothConnectionReceiver extends BroadcastReceiver {
+
     public BluetoothConnectionReceiver(){
-        //No initialisation code needed
     }
 
     @Override
     public void onReceive(Context context, Intent intent){
+        String action = intent.getAction();
+        // When discovery finds a device
+        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+            // Get the BluetoothDevice object from the Intent
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            // Add the name and address to an array adapter to show in a ListView
+            MainActivity.mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            Log.d("mArrayAdapter", MainActivity.mArrayAdapter.getItem(MainActivity.mArrayAdapter.getCount() -1).toString());
+        }
         /*if(BluetoothDevice.ACTION_ACL_CONNECTED.equals(action.getAction())){
             //Do something with bluetooth device connection
             log.d("ACTION_ACL_CONNECTED");
