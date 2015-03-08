44cf6dac8fc3e34380b33508393ddcea818f60eb
Ernest Wong <ewong2222@hotmail.com>
2015-03-08 14:50:19 -0400

    basic layout for main page

---
+++ SideKick-Android/app/src/main/java/com/example/ernestwong/sidekick/PatientActivity/java.java
@@ -0,0 +1,8 @@
+package com.example.ernestwong.sidekick.PatientActivity;
+
+import android.app.Activity;
+
+public class PatientActivity extends Activity {
+
+
+}
--- SideKick-Android/app/src/main/java/com/example/ernestwong/sidekick/WelcomeActivity.java
+++ SideKick-Android/app/src/main/java/com/example/ernestwong/sidekick/WelcomeActivity.java
@@ -3,15 +3,14 @@ package com.example.ernestwong.sidekick;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-
+import android.os.Handler;
 public class WelcomeActivity extends Activity {
     private final int SPLASH_DISPLAY_LENGTH = 1000;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-        setContentView(R.layout.splashscreen);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_main);
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
@@ -19,9 +18,9 @@ public class WelcomeActivity extends Activity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this,Menu.class);
-                Splash.this.startActivity(mainIntent);
-                Splash.this.finish();
+                Intent mainIntent = new Intent(WelcomeActivity.this, MainActivity.class);
+                WelcomeActivity.this.startActivity(mainIntent);
+                WelcomeActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
